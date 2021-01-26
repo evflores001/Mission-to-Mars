3d11eb2f5071005fe3819e1f63fc650e44f82a0d
evflores001 <67989268+evflores001@users.noreply.github.com>
2021-01-25 23:37:56 -0800

    Updated scrappy and Delieverable

--- scraping.py
+++ scraping.py
@@ -16,7 +16,7 @@ def scrape_all():
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
         "last_modified": dt.datetime.now(),
-        "listdict": hem_scrape(browser)
+        "listdict": hemi_img(browser)
     }
 
     # Stop webdriver and return data
@@ -59,28 +59,31 @@ def featured_image(browser):
     browser.visit(url)
 
     # Find and click the full image button
-    full_image_elem = browser.find_by_id('full_image')[0]
+    full_image_elem = browser.find_by_xpath('/html/body/div/div/div/main/div[2]/div[2]/div[2]/div/section/div[4]/div/a')
     full_image_elem.click()
 
     # Find the more info button and click that
-    browser.is_element_present_by_text('more info', wait_time=1)
-    more_info_elem = browser.links.find_by_partial_text('more info')
+    browser.is_element_present_by_xpath('/html/body/div/div/div/main/div/div[2]/div/div/div[2]/button', wait_time=1)
+    more_info_elem = browser.find_by_xpath('/html/body/div/div/div/main/div/div[2]/div/div/div[2]/button')
     more_info_elem.click()
 
     # Parse the resulting html with soup
     html = browser.html
     img_soup = soup(html, 'html.parser')
+    
 
     # Add try/except for error handling
     try:
         # Find the relative image url
-        img_url_rel = img_soup.select_one('figure.lede a img').get("src")
+        browser.is_element_present_by_xpath('/html/body/div/div/div/main/div/div[2]/div/div/div[3]/div[2]/div[2]/div/div/img', wait_time=1)
+        marsImage = browser.find_by_xpath('/html/body/div/div/div/main/div/div[2]/div/div/div[3]/div[2]/div[2]/div/div/img')['src']
 
     except BaseException:
         return None
 
     # Use the base url to create an absolute url
-    img_url = f'https://www.jpl.nasa.gov{img_url_rel}'
+    img_url = f'https://www.jpl.nasa.gov{marsImage}'
+    
 
     return img_url
 
@@ -100,62 +103,43 @@ def mars_facts():
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html(classes="table table-striped")
 
-def hem_scrape(browser):
+def hemi_image(browser):
 
     # 1. Use browser to visit the URL 
     url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+
     browser.visit(url)
-
-    # Get BS4 working
-    html = browser.html
-    moons_soup = soup(html, 'html.parser')
-
-    # Find all the URLs and put into a list
-    hemis = moons_soup.find_all("a", class_="itemLink product-item")
-
-    hemilist = []
-
-    for hemi in hemis:
-        hemilist.append(hemi.get("href"))
-
-    # Remove the duplicates and put back into a unique list
-    hemilist = np.unique(hemilist)
-    hemisp = hemilist.tolist()
 
     # 2. Create a list to hold the images and titles.
     hemisphere_image_urls = []
     site = 'https://astrogeology.usgs.gov/'
     # 3. Write code to retrieve the image urls and titles for each hemisphere.
-
-
-    # Write for loop to iterate through the tags/CSS elements
-
-    for hemi in hemisp:
-            
-        # Create dictionary object inside the for loop
-        hem_dict = {}    
+    for hemi in range(4):
         
+        # Create dictionary object inside the for loop
+        hemi_dict = {}    
+        keys = range(2)
         # click the link
-        browser.visit(f"{site}{hemi}")
-      
+        link = browser.find_by_tag("div.description a.itemLink.product-item")[hemi]
+        link.click()
+        
         # Reset the parser
         html2 = browser.html
-        moons_soup = soup(html2, 'html.parser')
+        image_soup = soup(html2, 'html.parser')
         
         # get the image link for the full sized image
-        imgtarget = moons_soup.select_one('li a').get('href')
+        image_target = image_soup.select_one('li a').get('href')
         
         # get the title for the image
-        imgtitle = moons_soup.select_one('h2').get_text()
-        
-        # Update the dictionary with this info
-        hem_dict.update({'img_url': imgtarget, 'title':imgtitle})
-        
-        # Append the dictionary to the URL list
-        hemisphere_image_urls.append(hem_dict)
-            
+        image_title = image_soup.select_one('h2').get_text()
+        values = [image_target, image_title]
         # For loop will: Click on each link, navigate to full-res image and pull image URL string and title for each image
-        browser.back()
+        for key in keys:
+            hemi_dict[key] = values[key]
+            hemi_dict['img_url'] = hemi_dict.pop(0)
+            hemi_dict['title'] = hemi_dict.pop(1)
+            hemisphere_image_urls.append(hemi_dict)
+            browser.back()
 
     return hemisphere_image_urls
 
